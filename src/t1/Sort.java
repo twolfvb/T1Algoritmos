17c060a37469a93d17c079eb0092ed5d6ec80298
twolfvb <tomas.wolf@ing.uchile.cl>
2014-05-02 14:14:12 -0400

    Ya, corrí tests. Eso si, en 2^20 es la paja insert y bubblesort. Viendo en todos los niveles, vi que bubblesort toma alrededor de 5 veces mas TIEMPO que insertsort, y hacen ambos un numero constante de comparaciones, en bubblesort justo es exactamente el doble que insertsort En 2^20 insertsort tomó alrededor de 491 segundos (8 minutos y fap) por loq eu bubble hubiera tomado 40, así que lo detuve. Quicksort y mergesort tomaron menos de 1 segundo en 2^20

--- src/t1/Sort.java
+++ src/t1/Sort.java
@@ -1,7 +1,7 @@
 package t1;
 
 public class Sort {
-	static double comparaciones;
+	static long comparaciones;
 	
 	//BubleSort
 	public static int[]  bubbleSort(int[] A){
@@ -116,26 +116,52 @@ public class Sort {
 	}	
 	public static void main(String[] args) {
 		
-		int n = 1024;
-		int[] A = Useful.generadorAleatorio(n);
-		Useful.printArray(A);
+		int n = (int)Math.pow(2,20);
+		int[] A = Useful.generadorCasiOrdenado(n, 5);
+		//Useful.printArray(A);
 		int[] B = new int[n];
-		int[] C = new int[n];
-		int[] D = new int[n];
-		int[] E = new int[n];
 		Useful.copyArray(A, B);
+		int[] C = new int[n];
 		Useful.copyArray(A, C);
+		int[] D = new int[n];
 		Useful.copyArray(A, D);
-		Useful.copyArray(A, E);
-		Useful.printArray(B);
-
-		bubbleSort(B);
-		insertSort(C);
-		mergeSort(A,0,n);
-		Useful.printArray(B);
-		Useful.printArray(A);
+		comparaciones = 0;
 		
-		System.out.println("byron weko");
+		long start = System.nanoTime();
+		//insertSort(A);
+		//System.out.println(comparaciones);
+		long end = System.nanoTime();
+		long microseconds = (end - start) / 1000;
+		//System.out.println(microseconds);
+		//System.out.println("InsertSort seconds = " + microseconds/1000000);
+		
+		/*comparaciones = 0;
+		start = System.nanoTime();
+		bubbleSort(B);
+		System.out.println(comparaciones);
+		end = System.nanoTime();
+		microseconds = (end - start) / 1000;
+		System.out.println(microseconds);
+		System.out.println("BubbleSort seconds = " + microseconds/1000000);
+		*/
+		
+		comparaciones = 0;
+		start = System.nanoTime();
+		mergeSort(C,0,n);
+		System.out.println(comparaciones);
+		end = System.nanoTime();
+		microseconds = (end - start) / 1000;
+		System.out.println(microseconds);
+		System.out.println("MergeSort seconds = " + microseconds/1000000);
+		
+		comparaciones = 0;
+		start = System.nanoTime();
+		quickSort(D, 0, n-1);
+		System.out.println(comparaciones);
+		end = System.nanoTime();
+		microseconds = (end - start) / 1000;
+		System.out.println(microseconds);
+		System.out.println("QuickSort seconds = " + microseconds/1000000);		
 	}
 	
 }
